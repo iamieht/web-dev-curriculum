6b30273eb97b0596d6532507a651e9d7f8f78471
Iván E. Hernández T <iamieht@gmail.com>
2022-07-11 15:27:06 +0200

    Create operators.js
    operators

---
+++ Meta_Front-End_Developer/Programming_with_JavaScript/Week1/operators.js
@@ -0,0 +1,39 @@
+// AND &&
+var currentTime = 10;
+console.log(currentTime > 9 && currentTime < 17);
+
+// OR ||
+var currentTime = 7;
+console.log(currentTime < 9 || currentTime > 17);
+
+// NOT !
+var petHungry = true;
+petHungry = !petHungry;
+console.log(petHungry);
+
+// Modulus %
+console.log(22 % 5);
+
+// equality ==
+console.log(5 == "5");
+
+// strict equality ===
+console.log(5 === "5");
+
+// + addition and concatenation
+
+// +=
+var overtime = 1;
+overtime += 2;
+overtime += 1;
+overtime += 2;
+overtime += 3;
+console.log(overtime);
+
+var longString = "";
+longString += "Once";
+longString += " upon";
+longString += " a";
+longString += " time";
+longString += "...";
+console.log(longString);
