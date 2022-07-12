19ac52ede114dbb144180a65576ca352654ba31d
Iván E. Hernández T <iamieht@gmail.com>
2022-07-12 12:47:40 +0200

    Create loops.js
    Exercise loops

---
+++ Meta_Front-End_Developer/Programming_with_JavaScript/Week1/loops.js
@@ -0,0 +1,37 @@
+// Task 1
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
+console.log("Counting completed!");
+
+// Task 2
+for (let i = 5; i > 0; i--) {
+  console.log(i);
+}
+console.log("Countdown finished!");
+
+// Task 3
+i = 1;
+
+while (i < 6) {
+  console.log(i);
+  i++;
+}
+console.log("Counting completed!");
+
+// Task 4
+i = 5;
+
+while (i > 0) {
+  console.log(i);
+  i--;
+}
+console.log("Countdown finished!");
+
+// Task 5
+year = 2018;
+
+while (year < 2023) {
+  console.log(year);
+  year++;
+}
