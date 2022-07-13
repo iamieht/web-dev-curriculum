78e40fb26840ff4d7e10e43deb643812f9c783d3
Iván E. Hernández T <iamieht@gmail.com>
2022-07-13 14:23:40 +0200

    Create arrays_and_objects.js
    Arrays and Objects

---
+++ Meta_Front-End_Developer/Programming_with_JavaScript/Week2/arrays_and_objects.js
@@ -0,0 +1,15 @@
+// Array
+var clothes = [];
+clothes.push("Pants");
+clothes.push("T-Shirt");
+clothes.push("Shirt");
+clothes.push("Underwear");
+clothes.push("Socks");
+clothes.pop();
+console.log(clothes[2]);
+
+// Object
+var favCar = {};
+favCar.color = "White";
+favCar.covertible = false;
+console.log(favCar);
