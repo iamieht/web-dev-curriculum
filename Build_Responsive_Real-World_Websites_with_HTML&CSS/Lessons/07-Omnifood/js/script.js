5aa4779ca917ded31b74f2edb8561d14d7932501
Iván E. Hernández T <iamieht@gmail.com>
2022-09-16 14:40:13 +0200

    Mobile navigation

--- Build_Responsive_Real-World_Websites_with_HTML&CSS/Lessons/07-Omnifood/js/script.js
+++ Build_Responsive_Real-World_Websites_with_HTML&CSS/Lessons/07-Omnifood/js/script.js
@@ -23,6 +23,14 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+// Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
