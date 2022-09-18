81fd76ba4a1a249b534ae97a1df2227b372a4579
Iván E. Hernández T <iamieht@gmail.com>
2022-09-18 09:44:49 +0200

    Sticky navigation

--- Build_Responsive_Real-World_Websites_with_HTML&CSS/Lessons/07-Omnifood/js/script.js
+++ Build_Responsive_Real-World_Websites_with_HTML&CSS/Lessons/07-Omnifood/js/script.js
@@ -56,6 +56,27 @@ allLinks.forEach(function (link) {
   });
 });
 
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
