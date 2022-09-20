3f05359bdd1dbe7919e2d8fd7d0cb03852cc3d68
Iván E. Hernández T <iamieht@gmail.com>
2022-09-20 15:15:28 +0200

    Update script.js
    Data Types

--- The_Complete_JavaScript_Course_2022/01-Fundamentals-Part-1/script.js
+++ The_Complete_JavaScript_Course_2022/01-Fundamentals-Part-1/script.js
@@ -12,3 +12,12 @@ console.log(
   "Population:",
   population
 );
+
+// Data Types
+// 1.Number: floating point numbers
+// 2.String
+// 3.Boolean
+// 4.Undefined: value when a variable is not yet defined let children;
+// 5.Null: empty value
+// 6.Symbol: value that is unique ans cannot be changed.
+// 7. BigInt: Larger integers
