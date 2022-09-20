e2efbe52598a647abd7a0b03944588ec93f0d7f2
Iván E. Hernández T <iamieht@gmail.com>
2022-09-20 13:14:06 +0200

    Update script.js
    Assignment 1 - Values and Variables

---
+++ The_Complete_JavaScript_Course_2022/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,14 @@
+// Assignment 1
+// Values and Variables
+let country = "Venezuela";
+let continent = "America";
+let population = "30MM";
+
+console.log(
+  "Country:",
+  country,
+  "Continent:",
+  continent,
+  "Population:",
+  population
+);
