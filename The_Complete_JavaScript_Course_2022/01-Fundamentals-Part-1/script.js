6cbc611662b135cc71aeb056b930907b670d4ddb
Iván E. Hernández T <iamieht@gmail.com>
2022-09-20 18:07:52 +0200

    Update script.js
    challenge #1

--- The_Complete_JavaScript_Course_2022/01-Fundamentals-Part-1/script.js
+++ The_Complete_JavaScript_Course_2022/01-Fundamentals-Part-1/script.js
@@ -22,11 +22,35 @@ console.log(
 // 5.Null: empty value
 // 6.Symbol: value that is unique ans cannot be changed.
 // 7.BigInt: Larger integers
-let isIsland = false;
-let language;
+const isIsland = false;
+const language = "Spanish";
 console.log(
   typeof isIsland,
   typeof population,
   typeof country,
   typeof language
 );
+
+// let, const, var
+// let to declare variables that will change
+// const to declare variables that won't change
+// var is legacy
+
+// Basic operators
+// +, -, *, /, **. =. +=. -=, *=, /=. ++. --
+// Comparison operators
+// >, <, >=, <=, == (without type), === (with type)
+
+// Coding Challenge #1
+const marksMass = 78;
+const marksHeight = 1.69;
+const johnsMass = 92;
+const johnsHeight = 1.95;
+
+const marksBMI = marksMass / marksHeight ** 2;
+const johnsBMI = johnsMass / johnsHeight ** 2;
+
+const markHigherBMI = marksBMI > johnsBMI;
+
+console.log("Mark BMI is:", marksBMI, "Johns BMI is:", johnsBMI);
+console.log("Is Marks BMI higher than Johns? :", markHigherBMI);
