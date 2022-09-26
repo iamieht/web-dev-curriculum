e6a6686edfc361d826f4ba1eb7b7b05dee4e472e
Iván E. Hernández T <iamieht@gmail.com>
2022-09-26 18:47:57 +0200

    Update script.js
    Arrow functions

--- The_Complete_JavaScript_Course_2022/02-Fundamentals-Part-2/script.js
+++ The_Complete_JavaScript_Course_2022/02-Fundamentals-Part-2/script.js
@@ -1,1 +1,12 @@
 "use strict"; // activating strict mode for more secure code
+
+// Arrow Function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
