7ef7ba663c86bf4bae299de55d83f4f73cda282c
Iván E. Hernández T <iamieht@gmail.com>
2022-09-26 19:44:28 +0200

    Coding Challenge #1
    Coding Challenge #1

--- The_Complete_JavaScript_Course_2022/02-Fundamentals-Part-2/script.js
+++ The_Complete_JavaScript_Course_2022/02-Fundamentals-Part-2/script.js
@@ -10,3 +10,20 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 };
 
 console.log(yearsUntilRetirement(1991, "Jonas"));
+
+// Coding Challenge #1
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+
+function checkWinner(team1, team2) {
+  if (team1 >= 2 * team2) {
+    console.log(`Team 1 wins (${team1} vs. ${team2})`);
+  } else if (team2 >= 2 * team1) {
+    console.log(`Team 2 wins (${team2} vs. ${team1})`);
+  } else {
+    console.log(`No team wins`);
+  }
+}
+
+checkWinner(avgDolphins, avgKoalas);
