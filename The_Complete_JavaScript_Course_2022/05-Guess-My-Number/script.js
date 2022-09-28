ce04dfc5d7f9bf7998f8f4219c02fd6f4413ed88
Iván E. Hernández T <iamieht@gmail.com>
2022-09-28 15:58:11 +0200

    Update script.js
    Play again!

--- The_Complete_JavaScript_Course_2022/05-Guess-My-Number/script.js
+++ The_Complete_JavaScript_Course_2022/05-Guess-My-Number/script.js
@@ -3,18 +3,19 @@
 // Functions
 
 // Define secret number
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-// Event Handler
+// Event Handlers
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // Game Logic
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
@@ -36,4 +37,16 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+// Play again
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
 });
