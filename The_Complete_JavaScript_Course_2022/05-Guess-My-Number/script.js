d5a11df1d7c6748e9209d7e21daccf313721e8c5
Iván E. Hernández T <iamieht@gmail.com>
2022-09-28 14:05:25 +0200

    Update script.js
    event handler

--- The_Complete_JavaScript_Course_2022/05-Guess-My-Number/script.js
+++ The_Complete_JavaScript_Course_2022/05-Guess-My-Number/script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+// Functions
+
+// Event Handler
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!";
+  }
+});
