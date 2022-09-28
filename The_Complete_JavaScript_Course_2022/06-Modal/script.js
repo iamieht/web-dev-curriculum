b646828bacf361c5685fe848ac385e5f42bf94bd
Iván E. Hernández T <iamieht@gmail.com>
2022-09-28 18:32:15 +0200

    Update script.js
    final modal

--- The_Complete_JavaScript_Course_2022/06-Modal/script.js
+++ The_Complete_JavaScript_Course_2022/06-Modal/script.js
@@ -4,3 +4,29 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
+
+// Functions
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Event Handlers
+btnOpenModal.forEach((element) => {
+  element.addEventListener("click", openModal);
+});
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// ESC event
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
