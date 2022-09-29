842fda9aaa452df1461ff0f056d467a8ab98712b
Iván E. Hernández T <iamieht@gmail.com>
2022-09-29 15:36:37 +0200

    Update script.js
    scoping

--- The_Complete_JavaScript_Course_2022/08-Behind-the-Scenes/script.js
+++ The_Complete_JavaScript_Course_2022/08-Behind-the-Scenes/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you are a millenial, ${firstName}`;
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
