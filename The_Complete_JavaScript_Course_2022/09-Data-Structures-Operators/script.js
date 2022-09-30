15db7dd0854c9da34f077065f97bbe415e4f341a
Iván E. Hernández T <iamieht@gmail.com>
2022-09-30 18:33:23 +0200

    Update script.js
    Destructuring

--- The_Complete_JavaScript_Course_2022/09-Data-Structures-Operators/script.js
+++ The_Complete_JavaScript_Course_2022/09-Data-Structures-Operators/script.js
@@ -30,4 +30,41 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+// Destructure Objects
+const { name, openingHours, categories } = restaurant;
+console.log(name.openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
