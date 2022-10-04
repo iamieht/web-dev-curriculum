9f9dc10bfd82e873ac07582a0f59c44a0a7f5687
Iván E. Hernández T <iamieht@gmail.com>
2022-10-04 19:17:39 +0200

    Update script.js
    Logical assignment operators

--- The_Complete_JavaScript_Course_2022/09-Data-Structures-Operators/script.js
+++ The_Complete_JavaScript_Course_2022/09-Data-Structures-Operators/script.js
@@ -149,3 +149,32 @@ console.log(guests);
 
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
+
+// Logical assignment operators
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+// OR assignment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+// AND assignment operator
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
