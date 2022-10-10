f3af0149fd40e7c7184d681d3d9eabf8f751015f
Iván E. Hernández T <iamieht@gmail.com>
2022-10-10 18:11:43 +0200

    Update script.js
    array methods

--- The_Complete_JavaScript_Course_2022/11-Arrays-Bankist/script.js
+++ The_Complete_JavaScript_Course_2022/11-Arrays-Bankist/script.js
@@ -74,3 +74,8 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2)); // does not mutate the array
+
+console.log(arr.splice(2)); // mutates the array
