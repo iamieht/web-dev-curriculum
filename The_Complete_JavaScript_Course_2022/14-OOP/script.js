729618b292ca676a1a6482a4a6e0442471c8bf28
Iván E. Hernández T <iamieht@gmail.com>
2022-10-26 15:16:47 +0200

    Update script.js
    Prototypes

--- The_Complete_JavaScript_Course_2022/14-OOP/script.js
+++ The_Complete_JavaScript_Course_2022/14-OOP/script.js
@@ -8,3 +8,10 @@ const Person = function (firstName, birthYear) {
 
 const jonas = new Person('Jonas', 1991);
 console.log(jonas);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
