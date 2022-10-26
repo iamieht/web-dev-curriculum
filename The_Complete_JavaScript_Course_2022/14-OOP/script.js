42d33175d059f99d3fbdf07e3955ddf8bf16b763
Iván E. Hernández T <iamieht@gmail.com>
2022-10-26 16:28:56 +0200

    Update script.js
    Coding challenge #2

--- The_Complete_JavaScript_Course_2022/14-OOP/script.js
+++ The_Complete_JavaScript_Course_2022/14-OOP/script.js
@@ -71,7 +71,44 @@ class PersonCL {
   get fullName() {
     return this._fullName;
   }
+
+  static hey() {
+    console.log('Hey there!');
+  }
 }
 
 const jessica = new PersonCL('Jessica', 1996);
 console.log(jessica.age);
+
+// Coding challenge #2
+class CarCL {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCL('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
