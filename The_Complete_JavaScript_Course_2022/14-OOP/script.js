578e70d4b30fb2e3ff4b16131d709f3086652bc1
Iván E. Hernández T <iamieht@gmail.com>
2022-10-26 15:07:35 +0200

    Update script.js
    Constructor functions

--- The_Complete_JavaScript_Course_2022/14-OOP/script.js
+++ The_Complete_JavaScript_Course_2022/14-OOP/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Constructor functions
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
